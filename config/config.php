0942afc0bf2e19427f49424267a8070f33a8a812
hugh.li <hugh.li@foxmail.com>
2021-04-20 16:04:27 +0800

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/4/18
+ * Time: 10:26 下午
+ */
+
+return [
+    //'default' => 'default',
+
+    /*
+     * default item
+     */
+    //'defaults' => [],
+
+    //'clients' => ['default' => []]
+];
---
+++ src/ServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/4/18
+ * Time: 10:32 下午
+ */
+
+namespace HughCube\Package;
+
+use Illuminate\Foundation\Application as LaravelApplication;
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Boot the provider.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Setup the config.
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(dirname(__DIR__) . '/config/config.php');
+
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('package.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('package');
+        }
+
+        $this->mergeConfigFrom($source, 'package');
+    }
+
+    /**
+     * Register the provider.
+     */
+    public function register()
+    {
+        $this->setupConfig();
+
+        $this->app->singleton(
+            Package::class,
+            function ($app) {
+                $config = $app->make('config')->get('package', []);
+            }
+        );
+    }
+}
